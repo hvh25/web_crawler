1f5e9dd40805870a15370abd0b5a0547c39711a9
hvh25 <hvh25@drexel.edu>
2013-03-13 20:03:28 -0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ WebCrawler::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
    config.action_mailer.raise_delivery_errors = true
 
