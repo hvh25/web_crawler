e9e26776e2aa616dfcab716ae43585eba07697aa
hvh25 <hvh25@drexel.edu>
2013-03-17 23:41:49 -0400

    dbdb

--- db/migrate/20130220235130_add_id_to_assignments.rb
+++ db/migrate/20130220235130_add_id_to_assignments.rb
@@ -1,6 +1,6 @@
 class AddIdToAssignments < ActiveRecord::Migration
   def change
-    add_column :assignments, :user_id, :string
-    add_column :assignments, :role_id, :string
+    add_column :assignments, :user_id, :integer
+    add_column :assignments, :role_id, :integer
   end
 end
