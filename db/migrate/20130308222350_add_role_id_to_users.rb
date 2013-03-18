4aa5c4ae063da4d67c2bbfa666f63a5b21a40f93
hvh25 <hvh25@drexel.edu>
2013-03-17 23:25:06 -0400

    db pg10

--- db/migrate/20130308222350_add_role_id_to_users.rb
+++
@@ -1,4 +0,0 @@
-class AddRoleIdToUsers < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20130309163013_add_source_id_to_job.rb
+++
@@ -1,6 +0,0 @@
-class AddSourceIdToJob < ActiveRecord::Migration
-  def change
-    add_column :jobs, :source_id, :integer
-    add_column :jobs, :source_type, :string
-  end
-end
