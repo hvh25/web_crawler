c2a762cf61049f0f4d8656322df3cc994ebb304e
hvh25 <hvh25@drexel.edu>
2013-03-17 22:13:34 -0400

    db pg6

--- db/migrate/20130307024915_change_description_format_in_job.rb
+++
@@ -1,8 +0,0 @@
-class ChangeDescriptionFormatInJob < ActiveRecord::Migration
-  def up
-    change_column :jobs, :description, :text
-  end
-	def down
-	    change_column :jobs, :description, :string
-	end
-end
