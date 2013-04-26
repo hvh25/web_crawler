9ba979c306edae6d9a46bc180bf23ebae3ec9e6e
hvh25 <hvh25@drexel.edu>
2013-04-26 00:23:41 -0400

    comment supperclass mismatch

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,4 @@
-class CommentsController < ApplicationController
+class CommentsController < ApplicationController::Base
   def index
   	@commentable = Job.find(params[:job_id])
     @owner = @commentable.user
