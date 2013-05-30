172e863c7e712035a5c9fe43b1b0813b995b636c
hvh25 <hvh25@drexel.edu>
2013-05-29 21:17:23 -0400

    search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -17,7 +17,7 @@ class SearchController < ApplicationController
       #@new = Job.order("created_at").last(100).sample(5)
       jobatjib = Job.all.select {|job| job.user != nil && job.availability >= Date.today}
       jobcrawl = Job.all.select {|job| job.user == nil && job.availability >= Date.today}
-      @new = jobatjib.sample(3) + jobcrawl.order("created_at").last(200).sample(2)
+      @new = jobatjib.sample(3) + jobcrawl.last(200).sample(2)
 #	end
  
     respond_to do |format|
